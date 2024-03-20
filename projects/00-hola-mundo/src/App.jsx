f43c8513ec6cf5917aa543f65f227de5b6384871
GuillermoDaniGarcia <163834885+GuillermoDaniGarcia@users.noreply.github.com>
2024-03-20 10:22:02 +0100

    last commit

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -1,24 +1,35 @@
+/* eslint-disable react/jsx-key */
+/* eslint-disable no-unused-vars */
 import './App.css'
 import { TwitterFollowCard } from './TwitterFollowCard'
+
+const users = [
+    {
+        userName: 'esthermn04',
+        name: 'esthermarquez_',
+        isFollowing: true
+    },
+    {
+        userName: 'FerlainL',
+        name: 'ferlain',
+        isFollowing: false
+    }
+]
 
 export function App(){
     return (
         <section className="App">
-            <TwitterFollowCard
-                userName="esthermn04"
-                name="esthermarquez_"
-                initialIsFollowing={true}
-            />
-
-            <TwitterFollowCard
-                userName="Alex__Fearnley"
-                name="Aleeee"
-            />
-
-            <TwitterFollowCard
-                userName="FerlainL"
-                name="ferlain"
-            />
+            {
+                users.map(({ userName, name, isFollowing}) =>(
+                        <TwitterFollowCard
+                            key={userName}
+                            userName={userName}
+                            name={name}
+                            initialIsFollowing={isFollowing}
+                        >
+                        </TwitterFollowCard>
+                    ))
+            }
         </section>
     )
 }
